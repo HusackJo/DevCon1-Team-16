9c368fa57ceb67cb273ec94233286f7d4b30d4c4
HusackJo <husackjo@gmail.com>
2024-09-21 17:13:57 -0400

    Added basic dragging & dropping
    added an object the player can move between grid components by clicking and dragging it around

--- My project (3)/Assets/Scripts/DraggableItem.cs
+++ My project (3)/Assets/Scripts/DraggableItem.cs
@@ -8,24 +8,32 @@ using UnityEngine.UI;
 
 /*Class for our items so we can drag and drop them. Note the:
  * using UnityEngine.EventSystems;
- * line to enable easy drag and drop functionality
- * 
+ * and
+ * using UnityEngine.UI;
+ * lines to enable easy drag and drop functionality
  */
 public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    //variable to be assigned
-    Transform parentAfterDrag;
+    //this variable is going to represent where our item locks to after the drag event
+    [HideInInspector] public Transform parentAfterDrag;
+    [SerializeField]
+    private Image imageRef;
 
     //
     public void OnBeginDrag(PointerEventData eventData)
     {
         //sets our placeholder to the spot it started in (on top of a UI square)
+        //we do this so the item has a parent to return to if it's not dropped anywhere meaningful.
         parentAfterDrag = transform.parent;
+
         //these 2 lines set our parent object to our canvas, then set us to the bottom of the canvas.
         //this so so we hover above the inventory UI no matter which slot the item is placed in.
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
-        Debug.Log("picked up!");
+
+        //Lastly, we disable Raycast Target on our image component so our drop detection doesnt always hit the image we drag.
+        //we'll re-enable this OnEndDrag so we can pick things up more than once, as well.
+        imageRef.raycastTarget = false;
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -37,6 +45,6 @@ public class DraggableItem : MonoBehaviour, IBeginDragHandler, IEndDragHandler, 
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.SetParent(parentAfterDrag);
-        Debug.Log("dropped");
+        imageRef.raycastTarget = true;
     }
 }
