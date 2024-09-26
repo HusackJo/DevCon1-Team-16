9608e31a0a85629e378ad12c16d6b7f2488c942f
StackOflow <132242531+StackOflow@users.noreply.github.com>
2024-09-25 23:36:02 -0400

    Made changes to PlayerAttack

--- My project (3)/Assets/2D Platformer/Scripts/PlayerAttack.cs
+++ My project (3)/Assets/2D Platformer/Scripts/PlayerAttack.cs
@@ -8,12 +8,14 @@ public class PlayerAttack : MonoBehaviour
 {
     private PlayerController PlayerMovement;
     private float CooldownTimer = Mathf.Infinity;
-    private float AttackCooldown;
+    public float attackCooldown;
+    private Transform firePoint;
+    private GameObject[] bullets;
     
     // Update is called once per frame
     private void Update()
     {
-        if (Input.GetMouseButton(0) && CooldownTimer > AttackCooldown && PlayerMovement.canAttack())
+        if (Input.GetMouseButton(0) && CooldownTimer > attackCooldown && PlayerMovement.canAttack())
         {
             Attack();
 
@@ -24,5 +26,8 @@ public class PlayerAttack : MonoBehaviour
     private void Attack()
     {
         CooldownTimer = 0;
+
+        bullets[0].transform.position = firePoint.position;
+        bullets[0].GetComponent<Bullet>().SetDirection(Mathf.Sign(transform.localscale.x));
     }
 }
