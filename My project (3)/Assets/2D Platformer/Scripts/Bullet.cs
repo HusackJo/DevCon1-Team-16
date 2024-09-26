76565d55c94f48e27b7b6a941091b40962f22253
StackOflow <132242531+StackOflow@users.noreply.github.com>
2024-09-25 22:47:50 -0400

    Updated Bullet so it destroys itself on collision

--- My project (3)/Assets/2D Platformer/Scripts/Bullet.cs
+++ My project (3)/Assets/2D Platformer/Scripts/Bullet.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    private float speed;
+    public float speed;
     private bool hit;
+    private float direction;
 
 
     private BoxCollider2D boxCollider;
@@ -18,12 +19,33 @@ public class Bullet : MonoBehaviour
     private void Update()
     {
         if (hit) return;
-        float movementSpeed = speed * Time.deltaTime;
+        float movementSpeed = speed * Time.deltaTime * direction;
         transform.Translate(movementSpeed, 0, 0);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        hit = true;
+        boxCollider.enabled = false;
+
+    }
+
+    public void SetDirection(float _direction)
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true; 
+
+        float localScaleX = transform.localScale.x;
+        if(Mathf.Sign(localScaleX) != _direction)
+            localScaleX =  -localScaleX;
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);   
     }
 }
